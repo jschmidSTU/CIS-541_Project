68e104670870e630ab04abcc84529e3f48c9e975
jschmidSTU <115262607+jschmidSTU@users.noreply.github.com>
2022-10-08 19:47:30 -0400

    Delete Accidents Streets.R

--- Accidents Streets.R
+++
@@ -1,6 +0,0 @@
-library(dplyr)
-
-accidents <- read.csv("/Users/julius.schmid/Desktop/Fundamentals of Big Data Analytics/Project/Files/US_Accidents_Dec20_updated.csv")
-miami_accidents <- filter(accidents, City == "Miami", State == "FL")
-
-tail(sort(table(miami_accidents$Street)),10)
